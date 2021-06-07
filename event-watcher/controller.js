c028e5d7b2bb9dadf967f379dce17ead5be7a1b9
CHEN MENG <354375204@qq.com>
2021-06-07 18:32:39 +0800

    中间节点.

--- event-watcher/controller.js
+++ event-watcher/controller.js
@@ -18,7 +18,7 @@ const connection = mysql.createConnection({
 // 跨链桥合约部署参数 管理员地址 是为了保持部署地址nonce为初始值 则每个链部署的跨链桥合约地址一样
 
 // 跨链桥地址
-const bridge_address = {
+const addressBridges = {
     // 7545: "",
     // 8545: "",
     qk: "",
@@ -38,7 +38,7 @@ const pk = {
 }
 
 // 跨链桥ABI
-const bridge_abi = [
+const abiBridge = [
     "event Deposit(string chain, address remote, address recipient, uint256 value)",
     "event WithdrawDone(address local, address recipient, uint256 value)",
     "function withdraw(string memory chain, address remote, address recipient, uint256 value)",
@@ -53,10 +53,6 @@ const urls = [
     // {
     //     name: 8545,
     //     url: "http://127.0.0.1:8545"
-    // }
-    // {
-    //     name: "Local",
-    //     url: "http://127.0.0.1:7545"
     // },
     // {
     //     name: "ETH",
@@ -98,39 +94,25 @@ let wallets = {}
 urls.forEach(item => {
     const provider = new ethers.providers.JsonRpcProvider(item.url)
     providers[item.name] = provider
-    wallets[item.name] = new ethers.Wallet(pk[item.name], provider);
+    wallets[item.name] = new ethers.Wallet(pk[item.name], provider)
 })
 
 // 设置跨链桥合约
 urls.forEach(item => {
-    bridgeContracts[item.name] = new ethers.Contract(bridge_address[item.name], bridge_abi, wallets[item.name])
+    bridgeContracts[item.name] = new ethers.Contract(addressBridges[item.name], abiBridge, wallets[item.name])
 })
 
-// 收件人是应付款的地址
-// 金额（以wei为单位）指定应发送的以太币数量
-// 随机数可以是任何唯一数字，以防止重放攻击
-// 合约地址 用于防止跨合约的重播攻击
-
 async function main() {
-    console.log(process.env.pk_heco)
     urls.forEach(item => {
-
-        // 转账筛选器
-        const filter = {
-            // https://docs.ethers.io/v5/api/providers/provider/#Provider--events
-            // https://docs.ethers.io/v5/api/providers/types/#providers-EventFilter
-            // address: "0x3Cf054A2c78A536373c8Fc998AA643B4072A4181",
-            topics: [
-                ethers.utils.id('Transfer(address,address,uint256)'),
-                null,
-                ethers.utils.hexZeroPad(bridge_address[item.name], 32) // 跨链桥合约地址
-            ]
-        }
-
         const contract = bridgeContracts[item.name]
+        contract.on("WithdrawDone", (local, recipient, value) => {
+            connection.connect()
+            connection.query("UPDATE bridge_logs SET isDone = ? WHERE ",[1])
+            connection.end()
+        })
+
         contract.on("Deposit", (chain, token, address, value) => {
             const _value = value.toString()
-            // 调用跨链桥转账
             const toContract = bridgeContracts[chain]
             if (toContract) {
                 toContract.withdraw(item.name, token, address, _value).then(_ => {
