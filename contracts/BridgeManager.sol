ad6e29bbbd5ae2208e334915d92e67299cb849bb
CHEN MENG <354375204@qq.com>
2021-07-28 17:16:05 +0800

    更新 支持 solidity 版本为0.8.6

--- contracts/BridgeManager.sol
+++ contracts/BridgeManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SimPL-2.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.6;
 
 interface Bridge {
     function withdraw(uint toChainId, address toToken, address recipient, uint256 value, bytes memory hash) external;
